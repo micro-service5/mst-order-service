79a3e4f7cb9519ccd4fc3c934d5f0ac11254a4bd
TANG ZhiXiong <tang.zhi.xiong@qq.com>
2018-01-18 19:20:45 +0800

    [tangzx] service discovery via consul

--- src/main/java/com/thoughtworks/mstorderservice/MstOrderServiceApplication.java
+++ src/main/java/com/thoughtworks/mstorderservice/MstOrderServiceApplication.java
@@ -2,9 +2,11 @@ package com.thoughtworks.mstorderservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @EnableFeignClients
 public class MstOrderServiceApplication {
 
